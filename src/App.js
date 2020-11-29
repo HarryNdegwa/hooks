281d3b863d998b0f7ee6e6ba5d8148de1b3d00ae
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-29 21:31:56 +0300

    Added useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import { useState } from "react";
 
 function App(props) {
+  const [count, setCount] = useState(10);
+
   return (
     <div className="App">
-      <h3>Helloo World</h3>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setCount((curCount) => curCount + 1);
+        }}
+      >
+        +
+      </button>
+      <p>{count}</p>
     </div>
   );
 }
